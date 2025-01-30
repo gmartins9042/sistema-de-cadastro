b893f2adcc1554af897fc4f0dcf5a025091015ed
gmartins9042 <164805436+gmartins9042@users.noreply.github.com>
2025-01-29 21:12:36 -0300

    adicionei 2 linhas

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos ")
 console.log("aplicação esta se conectando ao banco de dados")
+
+consoel.log("Fazendo um test com a minha segunda conta")
 consoel.log("Fazendo um test com a minha segunda conta")
